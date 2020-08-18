7930b1e3e133e03a1b4dbc7b13b0a00cf9485bbb
Lorrany Sousc <llorranysousc@gmail.com>
2020-08-18 16:52:16 -0300

    Create ex05.py

---
+++ ListaDeExercicios/ex05.py
@@ -0,0 +1,18 @@
+print('''5. Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+- A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+- A mensagem "Reprovado", se a média for menor do que sete;
+- A mensagem "Aprovado com Distinção", se a média for igual a dez.\n''')
+
+nome_aluno = str(input('Aluno: '))
+nota1 = float(input('1ª Nota: '))
+nota2 = float(input('2ª Nota: '))
+
+media = (nota1+nota2)/2
+#Testando possibilidades
+if media == 10:
+    situacao_aluno = 'APROVADO(a) com Distinção'
+elif media >= 7:
+    situacao_aluno = 'APROVADO(a)'
+else:
+    situacao_aluno = 'REPROVADO(a)'
+print(f'{nome_aluno} você está {situacao_aluno}!')
