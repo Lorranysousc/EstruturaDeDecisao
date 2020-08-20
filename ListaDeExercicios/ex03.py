dbb013340d785626f30eb3879155e2668aa446ea
Lorrany Sousc <llorranysousc@gmail.com>
2020-08-20 18:02:59 -0300

    Update ex03.py

--- ListaDeExercicios/ex03.py
+++ ListaDeExercicios/ex03.py
@@ -1,8 +1,6 @@
-print('3. Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.\n')
+'''3. Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.'''
 
 sexo_usuario = str(input('Qual seu sexo? ')).upper()[0] #.upper() vai transformar a entrada do usuário em letra maiúscula e o [0] vai pegar somente a primeira letra caso seja digitado uma palavra.
-
-#Testando possibilidades
 if sexo_usuario == 'F':
     sexo_usuario = 'Femino'
 elif sexo_usuario == 'M':
