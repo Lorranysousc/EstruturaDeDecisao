18c980a9d5b5eedf6f931e664f3c13dbee937740
Lorrany Sousc <llorranysousc@gmail.com>
2020-08-20 18:02:45 -0300

    Update ex01.py

--- ListaDeExercicios/ex01.py
+++ ListaDeExercicios/ex01.py
@@ -1,4 +1,4 @@
-print('1. Faça um Programa que peça dois números e imprima o maior deles.\n')
+'''1. Faça um Programa que peça dois números e imprima o maior deles.'''
 
 num1 = float(input('Digite o 1º número: '))
 num2 = float(input('Digite o 2º número: '))
