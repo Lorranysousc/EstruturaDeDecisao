fc1a6b96d0e5ec363471fd421626d6ded0e1804f
Lorrany Sousc <llorranysousc@gmail.com>
2020-08-20 18:03:15 -0300

    Create ex06.py

---
+++ ListaDeExercicios/ex06.py
@@ -0,0 +1,13 @@
+'''6. Faça um Programa que leia três números e mostre o maior deles.'''
+
+num1 = float(input('1º número: '))
+num2 = float(input('2º número: '))
+num3 = float(input('3º número: '))
+#Testando possibilidades
+if num2 < num1 > num3:
+    maior_numero = num1
+elif num1 < num2 > num3:
+    maior_numero = num2
+else:
+    maior_numero = num3
+print(f'O maior número é o {maior_numero}.')
