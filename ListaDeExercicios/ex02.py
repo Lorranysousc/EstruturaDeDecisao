5d29515ae95d9e1a7ce1906cbd9ddf8f0ad5400a
Lorrany Sousc <llorranysousc@gmail.com>
2020-08-20 18:02:52 -0300

    Update ex02.py

--- ListaDeExercicios/ex02.py
+++ ListaDeExercicios/ex02.py
@@ -1,4 +1,4 @@
-print('2. Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.\n')
+'''2. Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.'''
 
 entrada_usuario = float(input('Digite um número: '))
 
